06324dabd0e648bd289828126350ac825243b78e
Rajarshi Sen <rsen90@gmail.com>
2016-10-06 13:05:58 +0530

    init, resources: polls, models:Poll, views: :flow :new :show

---
+++ app/controllers/polls_controller.rb
@@ -0,0 +1,26 @@
+class PollsController < ApplicationController
+  def flow
+    @polls = Poll.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @poll = Poll.new
+  end
+
+  def create
+    @poll = Poll.new(poll_params)
+    if @poll.save
+      redirect_to @poll, notice: "Poll launched Successfully!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def poll_params
+    params.require(:poll).permit(:question)
+  end
+end
